4a6a17d28732056131019fd74f837f346b29f2f2
Fedja Bosnic <fedja.bosnic@ef.com>
2020-08-04 09:58:33 +0100

    Add cool code

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+resource "null_resource" "feature-flag" {
+    triggers = {
+      environment = var.environment
+      region      = var.region
+      salt        = base64sha512(file("${path.module}/../.secrets/password"))
+    }
 
+    provisioner "local-exec" {
+      command = "echo \"Flagging on ${var.environment}/${var.region}\""
+    }
+}
